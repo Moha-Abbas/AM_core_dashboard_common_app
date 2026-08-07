Moha-Abbas/core_dashboard_common_app

AM related modifications
AM related modifications

core_dashboard_common_app/static/core_dashboard_common_app/common/js/list/view_record.js
@@ -5,11 +5,28 @@
 openViewRecord = function() {
     var $registryRow = $(this).closest('tr');
     var objectID = $registryRow.attr("objectid");
-    var icon = $(this).find( "i" ).attr("class");
+    var viewBtn = $(this).find( "i" );
+    var icon = $(viewBtn).attr("class");
+
     // Show loading spinner
     showSpinner($(this).find("i"))
-    window.location = viewRecordUrl + '?id=' + objectID;
-    hideSpinner($(this).find("i"), icon)
+    $.ajax({
+        url: loadRecordUrl.replace("pk", objectID),
+        type: 'POST',
+        contentType: 'application/json',
+        success: function(response) {
+            sessionStorage.setItem('data_content', JSON.stringify(response.data_content));
+            sessionStorage.setItem('data_title', response.data_title);
+            sessionStorage.setItem('test_id', response.test_id);
+            window.location = '/gensel';
+        },
+        error: function(data){
+            let jsonResponse = JSON.parse(data.responseText);
+            $.notify(jsonResponse.error, "danger");
+        }
+    }).always(function() {
+        hideSpinner(viewBtn, icon)
+    });
 };
 
 $(".view-record-btn").on('click', openViewRecord);

Moha-Abbas/AM_core_dashboard_common_app

core_dashboard_common_app/static/core_dashboard_common_app/common/js/list/view_record.js
@@ -5,11 +5,28 @@
 openViewRecord = function() {
     var $registryRow = $(this).closest('tr');
     var objectID = $registryRow.attr("objectid");
-    var icon = $(this).find( "i" ).attr("class");
+    var viewBtn = $(this).find( "i" );
+    var icon = $(viewBtn).attr("class");
+
     // Show loading spinner
     showSpinner($(this).find("i"))
-    window.location = viewRecordUrl + '?id=' + objectID;
-    hideSpinner($(this).find("i"), icon)
+    $.ajax({
+        url: loadRecordUrl.replace("pk", objectID),
+        type: 'POST',
+        contentType: 'application/json',
+        success: function(response) {
+            sessionStorage.setItem('data_content', JSON.stringify(response.data_content));
+            sessionStorage.setItem('data_title', response.data_title);
+            sessionStorage.setItem('test_id', response.test_id);
+            window.location = '/gvform';
+        },
+        error: function(data){
+            let jsonResponse = JSON.parse(data.responseText);
+            $.notify(jsonResponse.error, "danger");
+        }
+    }).always(function() {
+        hideSpinner(viewBtn, icon)
+    });
 };
 
 $(".view-record-btn").on('click', openViewRecord);